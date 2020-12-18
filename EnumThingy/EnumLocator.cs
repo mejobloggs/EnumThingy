ac1aeb2b89c3306c7dfaeafb6a8340fb713f432a
Jonathan <Jonathan@DESKTOP-IMGO7CO>
2020-12-18 22:45:11 +1300

    initial

---
+++ EnumThingy/EnumLocator.cs
@@ -0,0 +1,42 @@
+﻿using EnumThingy.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumThingy
+{
+
+	public class EnumLocator
+	{
+		private string _assemblyName = "ExternalDllWithEnums";
+		private Assembly _assembly;
+
+		public EnumLocator()
+		{
+			string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+				+ $"\\{_assemblyName}.dll";
+
+			path = path.Replace("EnumThingy.Tests", "EnumThingy\\EnumThingy");
+
+			_assembly = Assembly.LoadFile(path);
+		}
+
+
+		public int GetEnumValue(string enumName, string enumValueName)
+		{
+			Type type = _assembly.GetType($"{_assemblyName}.Enums.{enumName}");
+			if (type?.IsEnum == true)
+			{
+				var result = Enum.Parse(type, enumValueName);
+				return (int)result;
+			}
+
+			throw new EnumNotFoundException($"Enum '{enumName}' not found");
+		}
+
+	}
+}
---
+++ ExternalDllWithEnums/Enums.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExternalDllWithEnums.Enums
+{
+	public enum OrangeCatEnum
+	{
+		Small = 1,
+		Large = 2,
+		Groovy = 3,
+		Orangeness = 5
+	}
+
+	public enum BoringCatEnum
+	{
+		MaximumSnooze = 1,
+		Yawn = 2,
+		Large = 3,
+		Small = 5
+	}
+
+	public enum SomeFoxEnum
+	{
+		Large = 1,
+		Catness = 2,
+		Dogness = 3,
+		Foxiness = 100,
+		Small = 101
+	}
+}
